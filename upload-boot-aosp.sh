dd1f022521e07c51716f649c24474549c18c6234
sakuramilk <c.sakuramilk@gmail.com>
2012-08-13 20:49:44 +0900

    build: upload script small change

--- upload-boot-aosp.sh
+++ upload-boot-aosp.sh
@@ -5,10 +5,10 @@ BINDIR=./out/AOSP/bin
 NOSU=$1
 
 if [ "$NOSU" = "nosu" ]; then
-#  adb push $BINDIR/boot.img /tmp/boot.img
-  adb push $BINDIR/boot.img /data/local/tmp/boot.img
-#  adb shell "cat /tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
-  adb shell "cat /data/local/tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
+  adb push $BINDIR/boot.img /tmp/boot.img
+#  adb push $BINDIR/boot.img /data/local/tmp/boot.img
+  adb shell "cat /tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
+#  adb shell "cat /data/local/tmp/boot.img > /dev/block/platform/msm_sdcc.1/by-name/boot"
   adb shell "rm /tmp/boot.img"
   adb shell "sync;sync;sync;sleep 2; reboot"
 else
