7decaf3079cec960c74458844f2cf3ee94e1cc81
ktoonsez <ktoonsez@gmail.com>
2012-09-03 23:38:29 +0900

    wakelock: (tungstwenty's implementation) plus lots of other  additions PART #2

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -17,7 +17,6 @@
 #include <linux/delay.h>
 #include <linux/workqueue.h>
 #include <linux/wakelock.h>
-#include "power.h"
 
 /* 
  * Timeout for stopping processes
@@ -159,10 +158,6 @@ int freeze_processes(void)
 		goto Exit;
 	printk("done.\n");
 
-	error = suspend_sys_sync_wait();
-	if (error)
-		goto Exit;
-
 	printk("Freezing remaining freezable tasks ... ");
 	error = try_to_freeze_tasks(false);
 	if (error)
@@ -208,4 +203,5 @@ void thaw_processes(void)
 	schedule();
 	printk("done.\n");
 }
+
 
