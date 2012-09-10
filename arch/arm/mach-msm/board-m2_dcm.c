3f42619e90c3e036c7ed005cb9a66592556f3cb9
HomuHomu <ecoco666@gmail.com>
2012-09-10 20:09:03 +0900

    fast charge: from imoseyon

--- arch/arm/mach-msm/board-m2_dcm.c
+++ arch/arm/mach-msm/board-m2_dcm.c
@@ -190,6 +190,7 @@ static unsigned char hdmi_is_primary = 1;
 #else
 static unsigned char hdmi_is_primary;
 #endif
+extern int force_fast_charge;
 
 static struct platform_device msm_fm_platform_init = {
 	.name = "iris_fm",
@@ -1608,7 +1609,8 @@ int msm8960_get_cable_type(void)
 #endif
 			break;
 		case CABLE_TYPE_USB:
-			fsa9485_usb_cb(1);
+			if (!force_fast_charge) fsa9485_usb_cb(1);
+				else fsa9485_charger_cb(1);
 			break;
 		case CABLE_TYPE_AC:
 			fsa9485_charger_cb(1);
