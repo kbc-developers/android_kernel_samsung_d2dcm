LineageOS/android_kernel_samsung_d2

d2: Import FMEM changes to board files
Change-Id: Ie47d4578c77be72704f431d0255aaf41bcfd197c

arch/arm/mach-msm/board-m2_att.c
@@ -1066,14 +1066,16 @@ static void __init msm8960_reserve(void)
 	msm_reserve();
 	if (fmem_pdata.size) {
 #if defined(CONFIG_ION_MSM) && defined(CONFIG_MSM_MULTIMEDIA_USE_ION)
-		fmem_pdata.phys = reserve_info->fixed_area_start +
-			MSM_MM_FW_SIZE;
-		pr_info("mm fw at %lx (fixed) size %x\n",
-			reserve_info->fixed_area_start, MSM_MM_FW_SIZE);
-		pr_info("fmem start %lx (fixed) size %lx\n",
-			fmem_pdata.phys, fmem_pdata.size);
-#else
-		fmem_pdata.phys = reserve_memory_for_fmem(fmem_pdata.size, fmem_pdata.align);
+    if (reserve_info->fixed_area_size) {
+        msm8960_fmem_pdata.phys =
+            reserve_info->fixed_area_start + MSM_MM_FW_SIZE;
+        pr_info("mm fw at %lx (fixed) size %x\n",
+        reserve_info->fixed_area_start, MSM_MM_FW_SIZE);
+        pr_info("fmem start %lx (fixed) size %lx\n",
+            msm8960_fmem_pdata.phys,
+            msm8960_fmem_pdata.size);
+    }
+}
 #endif
 	}
 	if (system_rev >= 16) {

arch/arm/mach-msm/board-m2_spr.c
@@ -1067,14 +1067,15 @@ static void __init msm8960_reserve(void)
 	msm_reserve();
 	if (fmem_pdata.size) {
 #if defined(CONFIG_ION_MSM) && defined(CONFIG_MSM_MULTIMEDIA_USE_ION)
-		fmem_pdata.phys = reserve_info->fixed_area_start +
-			MSM_MM_FW_SIZE;
-		pr_info("mm fw at %lx (fixed) size %x\n",
-			reserve_info->fixed_area_start, MSM_MM_FW_SIZE);
-		pr_info("fmem start %lx (fixed) size %lx\n",
-			fmem_pdata.phys, fmem_pdata.size);
-#else
-		fmem_pdata.phys = reserve_memory_for_fmem(fmem_pdata.size, fmem_pdata.align);
+    if (reserve_info->fixed_area_size) {
+        msm8960_fmem_pdata.phys =
+            reserve_info->fixed_area_start + MSM_MM_FW_SIZE;
+        pr_info("mm fw at %lx (fixed) size %x\n",
+        reserve_info->fixed_area_start, MSM_MM_FW_SIZE);
+        pr_info("fmem start %lx (fixed) size %lx\n",
+            msm8960_fmem_pdata.phys,
+            msm8960_fmem_pdata.size);
+    }
 #endif
 	}
 	if (system_rev >= 14) {

arch/arm/mach-msm/board-m2_vzw.c
@@ -1089,14 +1089,15 @@ static void __init msm8960_reserve(void)
 	msm_reserve();
 	if (fmem_pdata.size) {
 #if defined(CONFIG_ION_MSM) && defined(CONFIG_MSM_MULTIMEDIA_USE_ION)
-		fmem_pdata.phys = reserve_info->fixed_area_start +
-			MSM_MM_FW_SIZE;
-		pr_info("mm fw at %lx (fixed) size %x\n",
-			reserve_info->fixed_area_start, MSM_MM_FW_SIZE);
-		pr_info("fmem start %lx (fixed) size %lx\n",
-			fmem_pdata.phys, fmem_pdata.size);
-#else
-		fmem_pdata.phys = reserve_memory_for_fmem(fmem_pdata.size, fmem_pdata.align);
+    if (reserve_info->fixed_area_size) {
+        msm8960_fmem_pdata.phys =
+            reserve_info->fixed_area_start + MSM_MM_FW_SIZE;
+        pr_info("mm fw at %lx (fixed) size %x\n",
+        reserve_info->fixed_area_start, MSM_MM_FW_SIZE);
+        pr_info("fmem start %lx (fixed) size %lx\n",
+            msm8960_fmem_pdata.phys,
+            msm8960_fmem_pdata.size);
+    }
 #endif
 	}
 	if (system_rev >= 15) {

kbc-developers/android_kernel_samsung_d2dcm

arch/arm/mach-msm/board-m2_att.c
@@ -1062,16 +1062,18 @@ static void __init msm8960_reserve(void)
 	msm_reserve();
 	if (fmem_pdata.size) {
 #if defined(CONFIG_ION_MSM) && defined(CONFIG_MSM_MULTIMEDIA_USE_ION)
-		fmem_pdata.phys = reserve_info->fixed_area_start +
-			MSM_MM_FW_SIZE;
-		pr_info("mm fw at %lx (fixed) size %x\n",
+		if (reserve_info->fixed_area_size) {
+			fmem_pdata.phys =
+					reserve_info->fixed_area_start + MSM_MM_FW_SIZE;
+			pr_info("mm fw at %lx (fixed) size %x\n",
 			reserve_info->fixed_area_start, MSM_MM_FW_SIZE);
-		pr_info("fmem start %lx (fixed) size %lx\n",
-			fmem_pdata.phys, fmem_pdata.size);
-#else
-		fmem_pdata.phys = reserve_memory_for_fmem(fmem_pdata.size, fmem_pdata.align);
+			pr_info("fmem start %lx (fixed) size %lx\n",
+					fmem_pdata.phys,
+					fmem_pdata.size);
+		}
 #endif
-	}
+    }
+
 	if (system_rev >= 16) {
 		pr_err("Reserving memory at address %x size: %x\n", address, size);
 		ret = memblock_remove(address, size);

arch/arm/mach-msm/board-m2_spr.c
@@ -1062,16 +1062,18 @@ static void __init msm8960_reserve(void)
 	msm_reserve();
 	if (fmem_pdata.size) {
 #if defined(CONFIG_ION_MSM) && defined(CONFIG_MSM_MULTIMEDIA_USE_ION)
-		fmem_pdata.phys = reserve_info->fixed_area_start +
-			MSM_MM_FW_SIZE;
-		pr_info("mm fw at %lx (fixed) size %x\n",
+		if (reserve_info->fixed_area_size) {
+			fmem_pdata.phys =
+					reserve_info->fixed_area_start + MSM_MM_FW_SIZE;
+			pr_info("mm fw at %lx (fixed) size %x\n",
 			reserve_info->fixed_area_start, MSM_MM_FW_SIZE);
-		pr_info("fmem start %lx (fixed) size %lx\n",
-			fmem_pdata.phys, fmem_pdata.size);
-#else
-		fmem_pdata.phys = reserve_memory_for_fmem(fmem_pdata.size, fmem_pdata.align);
+			pr_info("fmem start %lx (fixed) size %lx\n",
+			fmem_pdata.phys,
+			fmem_pdata.size);
+		}
 #endif
 	}
+
 	if (system_rev >= 14) {
 		pr_err("Reserving memory at address %x size: %x\n", address, size);
 		ret = memblock_remove(address, size);

arch/arm/mach-msm/board-m2_vzw.c
@@ -1061,16 +1061,18 @@ static void __init msm8960_reserve(void)
 	msm_reserve();
 	if (fmem_pdata.size) {
 #if defined(CONFIG_ION_MSM) && defined(CONFIG_MSM_MULTIMEDIA_USE_ION)
-		fmem_pdata.phys = reserve_info->fixed_area_start +
-			MSM_MM_FW_SIZE;
-		pr_info("mm fw at %lx (fixed) size %x\n",
+		if (reserve_info->fixed_area_size) {
+			fmem_pdata.phys =
+					reserve_info->fixed_area_start + MSM_MM_FW_SIZE;
+			pr_info("mm fw at %lx (fixed) size %x\n",
 			reserve_info->fixed_area_start, MSM_MM_FW_SIZE);
-		pr_info("fmem start %lx (fixed) size %lx\n",
-			fmem_pdata.phys, fmem_pdata.size);
-#else
-		fmem_pdata.phys = reserve_memory_for_fmem(fmem_pdata.size, fmem_pdata.align);
+			pr_info("fmem start %lx (fixed) size %lx\n",
+					fmem_pdata.phys,
+					fmem_pdata.size);
+		}
 #endif
 	}
+
 	if (system_rev >= 15) {
 		pr_err("Reserving memory at address %x size: %x\n", address, size);
 		ret = memblock_remove(address, size);