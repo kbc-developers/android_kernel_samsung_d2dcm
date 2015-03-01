83b2a5cfb3729e2f06e7df84d006e93fa9186802
lawn <bloom.all.over.12@gmail.com>
2015-03-02 00:49:13 +0900

    Added SmartassH3 CPU Governor  for msm8974 pramerter

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -375,6 +375,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSH3)
+extern struct cpufreq_governor cpufreq_gov_smartass_h3;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass_h3)
 #endif
 
 
