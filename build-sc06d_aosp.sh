878e8f0d1b39dc09d27b8938b19ce0ae3985874e
lawn <bloom.all.over.12@gmail.com>
2016-02-10 00:26:19 +0900

    Delete Build System
    Change-Id: I44f775c589d5cd397064a3718d853d267a4adf4f

--- build-sc06d_aosp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export BUILD_TARGET=AOSP
-. sc06d.config
-
-time ./_build-bootimg.sh $1
--- build-sc06d_recovery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export BUILD_TARGET=RECO
-. sc06d.config
-
-time ./_prebuild-recoveryimg.sh
